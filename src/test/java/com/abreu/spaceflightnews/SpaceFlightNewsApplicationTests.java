ffa5f1d5334955f9e1b7f66d850d11f9d9594864
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2023-10-28 11:03:04 -0300

    Initial commit

---
+++ src/test/java/com/abreu/spaceflightnews/SpaceFlightNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abreu.spaceflightnews;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpaceFlightNewsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
