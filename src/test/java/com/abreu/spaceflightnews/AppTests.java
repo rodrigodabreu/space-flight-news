0cb74a61b00812c63d3830f0fba49a3f0db4dfd8
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2023-10-28 11:15:29 -0300

    chore: rename main class

--- src/test/java/com/abreu/spaceflightnews/AppTests.java
+++ src/test/java/com/abreu/spaceflightnews/AppTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpaceFlightNewsApplicationTests {
+class AppTests {
 
   @Test
   void contextLoads() {
